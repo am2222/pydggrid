630b6c16ce8b5ab19972067c87628c5504a3f10e
am2222 <asd56yu@gmail.com>
2019-06-02 16:31:36 -0400

    Appveyor windows build test

--- src/dggrid/pj_msfn.cpp
+++
@@ -1,6 +0,0 @@
-/* determine constant small m */
-#include <cmath>
-	long double
-pj_msfn(long double sinphi, long double cosphi, long double es) {
-	return (cosphi / std::sqrt (1. - es * sinphi * sinphi));
-}
